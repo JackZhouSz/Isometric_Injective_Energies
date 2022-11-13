1276adc1f2d8d399a17ca114604f9a3ecd88ab35
CharlesDu <dohoney@126.com>
2022-11-13 12:05:41 -0600

    mesh-injectivity-promoting energies

---
+++ src/Injective_Energy_2D_Formulation.cpp
@@ -0,0 +1,137 @@
+//
+// Created by Charles Du on 11/12/22.
+//
+
+#include "Injective_Energy_2D_Formulation.h"
+
+#include <utility>
+#include "geo_util.h"
+
+using namespace Eigen;
+
+Injective_Energy_2D_Formulation::Injective_Energy_2D_Formulation(Eigen::MatrixXd restV_, Eigen::Matrix2Xd initV,
+                                                                 Eigen::Matrix3Xi F_, const Eigen::VectorXi &handles)
+        : Energy_Formulation((initV.cols() - handles.size()) * initV.rows()),
+          F(std::move(F_)), V(std::move(initV)), restV(std::move(restV_)) {
+    // compute freeI: indices of free vertices
+    auto nV = restV.cols();
+    int vDim = 2;
+    std::vector<bool> freeQ(nV, true);
+    for (auto i = 0; i < handles.size(); ++i) {
+        freeQ[handles(i)] = false;
+    }
+    freeI.resize(nV - handles.size());
+    {
+        int ii = 0;
+        for (int i = 0; i < nV; ++i) {
+            if (freeQ[i]) {
+                freeI[ii] = i;
+                ++ii;
+            }
+        }
+    }
+    std::sort(freeI.data(), freeI.data() + freeI.size());
+
+    // experimental: compute free face Indices
+    // free face: a triangle with at least one free vertices
+    auto nF = F.cols();
+    int n_free_face = 0;
+    std::vector<bool> free_faceQ(nF, false);
+    for (int i = 0; i < nF; ++i) {
+        int i1 = F(0, i);
+        int i2 = F(1, i);
+        int i3 = F(2, i);
+        if (freeQ[i1] || freeQ[i2] || freeQ[i3]) {
+            free_faceQ[i] = true;
+            ++n_free_face;
+        }
+    }
+    free_faceI.resize(n_free_face);
+    free_face_areas.resize(n_free_face);
+    {
+        int ii = 0;
+        for (int i = 0; i < nF; ++i) {
+            if (free_faceQ[i]) {
+                free_faceI[ii] = i;
+                free_face_areas[ii] = compute_tri_signed_area(V.col(F(0,i)),
+                                                              V.col(F(1,i)),
+                                                              V.col(F(2,i)));
+                ++ii;
+            }
+        }
+    }
+
+    // compute indexDict and F_free
+    indexDict = VectorXi::Constant(nV, -1);
+    for (auto i = 0; i < freeI.size(); ++i) {
+        indexDict(freeI(i)) = i;
+    }
+    F_free.resize(F.rows(), F.cols());
+    for (auto i = 0; i < F.cols(); ++i) {
+        for (auto j = 0; j < F.rows(); ++j) {
+            F_free(j, i) = indexDict(F(j, i));
+        }
+    }
+
+    // compute x from V
+    curr_x.resize(vDim * freeI.size());
+    for (auto i = 0; i < freeI.size(); ++i) {
+        int vi = freeI(i);
+        for (int j = 0; j < vDim; ++j) {
+            curr_x(i * vDim + j) = V(j, vi);
+        }
+    }
+
+    // extract boundary edges
+    extract_mesh_boundary_edges(F, boundary_edges);
+    // check if the boundary is fixed
+    fixed_boundary = true;
+    for (const auto &edge: boundary_edges) {
+        if (freeQ[edge.first] || freeQ[edge.second]) {
+            fixed_boundary = false;
+            break;
+        }
+    }
+    // find boundary vertices
+    std::vector<bool> is_boundary_vert(V.cols(), false);
+    for (auto &e: boundary_edges) {
+        is_boundary_vert[e.first] = true;
+        is_boundary_vert[e.second] = true;
+    }
+    // build map: vert index -> boundary vert index
+    int n_boundary_vert = 0;
+    std::vector<int> bndry_vId_of_vert(V.cols(), -1);
+    for (int i = 0; i < is_boundary_vert.size(); ++i) {
+        if (is_boundary_vert[i]) {
+            bndry_vId_of_vert[i] = n_boundary_vert;
+            ++n_boundary_vert;
+        }
+    }
+    // collect boundary vertices
+    boundaryI.resize(n_boundary_vert);
+    {
+        int ii = 0;
+        for (int i = 0; i < bndry_vId_of_vert.size(); ++i) {
+            if (bndry_vId_of_vert[i] != -1) {
+                boundaryI(ii) = i;
+                ++ii;
+            }
+        }
+    }
+    // re-index boundary edges into boundary vertices
+    for (auto& e : boundary_edges) {
+        e.first = bndry_vId_of_vert[e.first];
+        e.second = bndry_vId_of_vert[e.second];
+    }
+}
+
+
+void Injective_Energy_2D_Formulation::update_x_and_V(const VectorXd &x) {
+    curr_x = x;
+    int vDim = 2;
+    for (auto i = 0; i < freeI.size(); ++i) {
+        for (auto j = 0; j < vDim; ++j) {
+            V(j, freeI(i)) = x[i * vDim + j];
+        }
+    }
+}
---
+++ src/Injective_Energy_2D_Formulation.h
@@ -0,0 +1,67 @@
+//
+// Created by Charles Du on 11/12/22.
+//
+
+#ifndef ISO_TLC_SEA_INJECTIVE_ENERGY_2D_FORMULATION_H
+#define ISO_TLC_SEA_INJECTIVE_ENERGY_2D_FORMULATION_H
+
+#include "Energy_Formulation.h"
+
+class Injective_Energy_2D_Formulation : public Energy_Formulation {
+public:
+    Injective_Energy_2D_Formulation(Eigen::MatrixXd restV_, Eigen::Matrix2Xd initV,
+                                    Eigen::Matrix3Xi F_, const Eigen::VectorXi &handles);
+
+    ~Injective_Energy_2D_Formulation() override = default;
+
+    double compute_energy(const Eigen::VectorXd &x, Eigen::VectorXd &energy_list) override = 0;
+
+    double compute_energy_with_gradient(const Eigen::VectorXd &x, Eigen::VectorXd &grad) override = 0;
+
+    double compute_energy_with_gradient_Hessian(const Eigen::VectorXd &x, Eigen::VectorXd &energy_list,
+                                                Eigen::VectorXd &grad, SpMat &Hess) override = 0;
+
+    // check whether the current mesh is injective.
+    // If so, store the current mesh.
+    bool is_injective() override = 0;
+
+    // get the mesh vertices when the last time the mesh is injective
+    Eigen::Matrix2Xd get_latest_injective_V() { return latest_injective_V; }
+
+    // get the current mesh vertices
+    Eigen::Matrix2Xd get_V() { return V; }
+
+protected:
+    // rest vertices
+    Eigen::MatrixXd restV;
+    // current V of target mesh
+    Eigen::Matrix2Xd V;
+    // V indices of triangles
+    Eigen::Matrix3Xi F;
+    // indices of free vertices
+    Eigen::VectorXi freeI;
+    // indices of free faces (i.e. face with at least one free vertex)
+    Eigen::VectorXi free_faceI;
+    // signed area of free faces
+    Eigen::VectorXd free_face_areas;
+    // map: V index --> freeV index. If V(i) is not free, indexDict(i) = -1.
+    Eigen::VectorXi indexDict;
+    // freeV indices of triangles.
+    Eigen::Matrix3Xi F_free;
+
+    // boundary vertex indices
+    Eigen::VectorXi boundaryI;
+    // boundary edges: each edge is a pair of indices of boundaryI
+    std::vector<std::pair<size_t, size_t>> boundary_edges;
+    // whether mesh boundary is fixed
+    bool fixed_boundary;
+
+    // V when the last time is_injective() is true
+    Eigen::Matrix2Xd latest_injective_V;
+
+    // update current x and V
+    void update_x_and_V(const Eigen::VectorXd &x);
+};
+
+
+#endif //ISO_TLC_SEA_INJECTIVE_ENERGY_2D_FORMULATION_H
