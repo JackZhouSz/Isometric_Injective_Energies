f5cfb254e682fe4677629d87e318d4dfb847eaea
CharlesDu <dohoney@126.com>
2023-04-25 07:27:36 -0500

    Dirichlet energy for triangle mesh

---
+++ src/Dirichlet_2D_Formulation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Charles Du on 4/25/23.
+//
+
+#include "Dirichlet_2D_Formulation.h"
---
+++ src/Dirichlet_2D_Formulation.h
@@ -0,0 +1,30 @@
+//
+// Created by Charles Du on 4/25/23.
+//
+
+#ifndef ISO_TLC_SEA_DIRICHLET_2D_FORMULATION_H
+#define ISO_TLC_SEA_DIRICHLET_2D_FORMULATION_H
+
+//#include <utility>
+
+#include "Distortion_Energy_2D_Formulation.h"
+
+class Dirichlet_2D_Formulation : public Distortion_Energy_2D_Formulation {
+public:
+    Dirichlet_2D_Formulation(Eigen::MatrixXd restV_, Eigen::Matrix2Xd initV,
+                             Eigen::Matrix3Xi F_, const Eigen::VectorXi &handles)
+                             : Distortion_Energy_2D_Formulation(std::move(restV_), std::move(initV), std::move(F_), handles) {};
+
+    ~Dirichlet_2D_Formulation() override = default;
+
+protected:
+    double compute_psi(double I1, double I2, double I3) final { return I2; }
+
+    double compute_psi_with_gradient(double I1, double I2, double I3, Eigen::Vector3d &grad_psi) final {
+        grad_psi << 0, 1, 0;
+        return I2;
+    }
+};
+
+
+#endif //ISO_TLC_SEA_DIRICHLET_2D_FORMULATION_H
